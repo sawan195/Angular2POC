198c7a024be0c504bf82089f3942bb83237dc3ab
anjalitanpure <anjali.tanpure@synerzip.com>
2016-12-28 12:00:51 +0530

    test

--- src/app/pop-up/pop-up.component.ts
+++ src/app/pop-up/pop-up.component.ts
@@ -1,5 +1,6 @@
 import {Component, ViewChild} from '@angular/core';
-import { ModalDirective } from 'ng2-bootstrap/ng2-bootstrap';
+import {ModalDirective} from "ng2-bootstrap";
+//import { ModalDirective } from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'my-pop-up',
